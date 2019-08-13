bfbc8285da13006ca442aeaff2ca9ec45479e68b
Henrik Holst <hholst80@gmail.com>
2019-08-13 10:32:47 +0200

    Update join.c

--- join.c
+++ join.c
@@ -15,44 +15,62 @@ main(int argc, char **argv)
 {
   char * sep = " ";
   int truncate_empty_records = 0;
+  int trailing_newline = 1;
+  size_t rc = 0;
   int c;
 
-  while ((c = getopt (argc, argv, "rd:")) != -1) {
+  while ((c = getopt (argc, argv, "hd:nr")) != -1) {
     switch (c) {
-      case 'r':
-        truncate_empty_records = 1;
-        break;
       case 'd':
         sep = optarg;
         break;
+      case 'h':
+        printf("Usage: join [OPTION]...\n"
+               "Join line records together on a single line delimited by spaces.\n"
+               "\n"
+               "   -d DELIMITER          use DELIMITER string as output record delimiter\n"
+               "   -n                    do not output the trailing newline\n"
+               "   -r                    ignore empty line records from input\n"
+               "\n"
+               "join source code: <https://github.com/cli-tools/join/>\n"
+               "join community: <https://gitter.im/cli-tools/community>\n"
+               );
+        exit(EXIT_SUCCESS);
+        break;
+      case 'n':
+        trailing_newline = 0;
+        break;
+      case 'r':
+        truncate_empty_records = 1;
+        break;
+      default:
+        fputs("Try 'join -h' for more information.\n", stderr);
+        exit(EXIT_FAILURE);
     }
   }
 
-  do
+  while (1)
   {
-    size_t rc = 0;
     char * record = NULL;
     size_t n = 0;
-    while (getline(&record, &n, stdin) > 0)
-    {
-      record[strcspn(record, "\r\n")] = 0; /* credit: https://stackoverflow.com/a/28462221/3504575 */
-      if (truncate_empty_records && strlen(record) == 0) {
-        free(record);
-        record = NULL;
-        n = 0;
-        continue;
-      }
-      rc++;
-      if (rc > 1) {
-        fputs(sep, stdout);
-      }
-      fputs(record, stdout);
-      free(record);
-      record = NULL;
-      n = 0;
+    if (getline(&record, &n, stdin) < 0) {
+      break;
     }
+    /* Credit: https://stackoverflow.com/a/28462221/3504575 */
+    record[strcspn(record, "\r\n")] = 0;
+    if (truncate_empty_records && strlen(record) == 0) {
+      free(record);
+      continue;
+    }
+    rc++;
+    if (rc > 1) {
+      fputs(sep, stdout);
+    }
+    fputs(record, stdout);
+    free(record);
   }
-  while (0);
-  fputc('\n', stdout);
-  return 0;
+  if (trailing_newline) {
+    fputc('\n', stdout);
+  }
+  exit(EXIT_SUCCESS);
 }
